ea8378b922f8829edc1391e344d68e9ae23c6375
ChiranjeeviKandukuri <kandukuri.chiranjeevi@valuemomentum.biz>
2019-05-02 16:11:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-3ro4d9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-3ro4d9)
---
+++ src/app/zippy.component.ts
@@ -0,0 +1,26 @@
+import {Component,EventEmitter,Output} from '@angular/core'
+
+@Component({
+  selector: 'zippy',
+  template: `
+  <div class="zippy">
+    <div (click)="toggle()">Toggle</div>Hi<br>
+    <div [hidden]="!visible">
+      <ng-content></ng-content>
+    </div>
+  </div>`})
+
+export class ZippyComponent {
+  visible = true;
+  @Output() open = new EventEmitter<any>();
+  @Output() close = new EventEmitter<any>();
+
+  toggle() {
+    this.visible = !this.visible;
+    if (this.visible) {
+      this.open.emit(this);
+    } else {
+      this.close.emit(this);
+    }
+  }
+}
